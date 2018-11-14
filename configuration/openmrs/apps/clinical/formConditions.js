fdd0e7904afd1f9b31ee1badbbac916eb8693661
rbuisson <romain@mekomsolutions.com>
2018-11-14 14:58:17 +0100

    GHC-20: Format formConditions.js with ';' add the end of each line

--- configuration/openmrs/apps/clinical/formConditions.js
+++ configuration/openmrs/apps/clinical/formConditions.js
@@ -5,11 +5,11 @@ Bahmni.ConceptSet.FormConditions.rules = {
         if (regex.test(bloodPressure)) {
             return {
                 enable: ["Posture"]
-            }
+            };
         } else {
             return {
                 disable: ["Posture"]
-            }
+            };
         }
     },
     'Diastolic Data' : function (formName, formFieldValues) {
@@ -17,9 +17,9 @@ Bahmni.ConceptSet.FormConditions.rules = {
         if (diastolic) {
             return {
                 enable: ["Posture"]
-            }
+            };
         } else {
-            return {}
+            return {};
         }
     },
     'Systolic Data' : function (formName, formFieldValues) {
@@ -27,43 +27,43 @@ Bahmni.ConceptSet.FormConditions.rules = {
         if (systolic) {
             return {
                 enable: ["Posture"]
-            }
+            };
         } else {}
     },
     'GHC_Other actions': function (formName, formFieldValues) {
-        var answer = formFieldValues['GHC_Other actions']
+        var answer = formFieldValues['GHC_Other actions'];
         if (answer) {
             return {
                 show: ['GHC_Non coded actions']
-            }
+            };
         } else {
             return {
                 hide: ['GHC_Non coded actions']
-            }
+            };
         }
     },
     'GHC_Ulcerations': function (formName, formFieldValues) {
-        var answer = formFieldValues['GHC_Ulcerations']
+        var answer = formFieldValues['GHC_Ulcerations'];
         if (answer) {
             return {
                 show: ['GHC_Ulcerations location']
-            }
+            };
         } else {
             return {
                 hide: ['GHC_Ulcerations location']
-            }
+            };
         }
     },
     'GHC_Vesicles': function (formName, formFieldValues) {
-        var answer = formFieldValues['GHC_Vesicles']
+        var answer = formFieldValues['GHC_Vesicles'];
         if (answer) {
             return {
                 show: ['GHC_Vesicles location']
-            }
+            };
         } else {
             return {
                 hide: ['GHC_Vesicles location']
-            }
+            };
         }
     }
 };
